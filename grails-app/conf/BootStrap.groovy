85cdc733319fb00e1dfdd5cce30dc5c3109ba979
AntoineWillems <willemsan.antoine@gmail.com>
2014-04-11 16:21:32 +0200

    Modification initialisation BDD

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,15 +8,26 @@ class BootStrap {
 		Livre livre
 		Auteur auteur
 		TypeDocument type
+		def listAuteur = []
 		File f =new File("C://Users/Willems/Documents/Cours/JEE/Bibliotheque/bdd.csv")
 		
 		f.toCsvReader(['separatorChar':'	']).eachLine { tokens ->
 			
 			type = new TypeDocument(intitule:tokens[1]).save()
-			livre = new Livre(titre:tokens[3], nombreExemplaires:1, nombreExemplairesDisponible:1).save()
+			//livre = new Livre(titre:tokens[3], nombreExemplaires:1, nombreExemplairesDisponible:1).save()
 			//livre = new Livre(titre:tokens[3], nombreExemplaires:1, nombreExemplairesDisponible:1).addToAuteur(new Auteur(nom:"Will", prenom:"Ant")).save()
-			auteur = new Auteur(nom:tokens[4], prenom:tokens[5]).save()
+			/*
+			listAuteur.each{ auteur1 ->
+				if(auteur1.getNom()==tokens[4]){
+					auteur1.addToLivres(new Livre(titre:tokens[3], nombreExemplaires:1, nombreExemplairesDisponible:1))
+				}
+			}
+			*/
 			
+			
+			
+			auteur = new Auteur(nom:tokens[4], prenom:tokens[5]).addToLivres(new Livre(titre:tokens[3], nombreExemplaires:1, nombreExemplairesDisponible:1)).save()
+			//listAuteur.add(auteur)
 		}
     }
     def destroy = {
