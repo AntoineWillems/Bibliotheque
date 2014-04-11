20ac82190121daa35b0fa44e2010537c7007b325
AntoineWillems <willemsan.antoine@gmail.com>
2014-04-11 15:43:03 +0200

    Changement initialisation BDD

--- grails-app/controllers/bibliotheque/LivreController.groovy
+++ grails-app/controllers/bibliotheque/LivreController.groovy
@@ -108,20 +108,4 @@ class LivreController {
             redirect(action: "show", id: id)
         }
     }
-	
-	def createBDD(){
-		Livre livre
-		Auteur auteur
-		TypeDocument type
-		File f =new File("C://Users/Willems/Documents/Cours/JEE/Bibliotheque/bdd.csv")
-		
-		f.toCsvReader(['separatorChar':'	']).eachLine { tokens ->
-			
-			type = new TypeDocument(intitule:tokens[1]).save()
-			livre = new Livre(titre:tokens[3], nombreExemplaires:1, nombreExemplairesDisponible:1).save()
-			//livre = new Livre(titre:tokens[3], nombreExemplaires:1, nombreExemplairesDisponible:1).addToAuteur(new Auteur(nom:"Will", prenom:"Ant")).save()
-			auteur = new Auteur(nom:tokens[4], prenom:tokens[5]).save()
-			
-		}
-	}
 }
