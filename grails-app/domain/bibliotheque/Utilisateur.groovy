9c81261ad8d28a09a2861fecf2c64dc7456b4369
Abdoul <khadrygassama@gmail.com>
2014-04-16 11:40:15 +0200

    Ajout de l'utilisateurController et UtiliateurViews

--- grails-app/domain/bibliotheque/Utilisateur.groovy
+++ grails-app/domain/bibliotheque/Utilisateur.groovy
@@ -4,4 +4,17 @@ package bibliotheque
  * Created by Gassama on 16/04/2014.
  */
 class Utilisateur {
+    String nom
+    String prenom
+    String adress
+    String mail
+    String login
+    String password
+
+    static belongsTo = [
+            panier : Panier
+    ]
+    static constraints = {
+
+    }
 }
